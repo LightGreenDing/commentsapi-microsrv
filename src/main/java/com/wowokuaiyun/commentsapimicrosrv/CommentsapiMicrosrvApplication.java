7c033dde8a424774ec97e46ebb772628bbb954fc
丁杰 <496328388@qq.com>
2018-12-04 11:47:59 +0800

    Initial commit

---
+++ src/main/java/com/wowokuaiyun/commentsapimicrosrv/CommentsapiMicrosrvApplication.java
@@ -0,0 +1,12 @@
+package com.wowokuaiyun.commentsapimicrosrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsapiMicrosrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommentsapiMicrosrvApplication.class, args);
+    }
+}
